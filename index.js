8772b37974130934fed08783a5f1664bc7cd682c
somehow55 <79687989+somehow55@users.noreply.github.com>
2022-11-11 18:10:57 +0900

    Add background images & update html, js
    1) add background images
    2) update html&js for random background show

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
             renderCurrentTime();
             renderQuote();
+            renderRandomImage();
         });
 
 
@@ -14,13 +15,26 @@ function renderCurrentTime() {
 }
 
 
-        function renderQuote() {
-            let url = `https://api.quotable.io/random`;
-            fetch(url)
-                .then(res => res.json()).then((data) => {
-                    let content = `" ${data['content']} "`;
-                    let author = `- ${data['author']} -`;
-                    $('#content').text(content);
-                    $('#author').text(author);
-                });
-        }
+function renderQuote() {
+    let url = `https://api.quotable.io/random`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let content = `" ${data['content']} "`;
+            let author = `- ${data['author']} -`;
+            $('#content').text(content);
+            $('#author').text(author);
+        });
+}
+
+
+//배경화면 랜덤화
+function renderRandomImage() {
+    let imageList = [];
+    for (i = 0; i < 9; i++) {
+      imageList.push(i);
+    }
+    let imageListLength = imageList.length;
+    let randomImage = Math.floor(Math.random() * (imageListLength))+1
+    randomImage = `backgroundImages/${randomImage}.jpg`
+    $(document.body).css("background-image", `url(${randomImage})`);
+  }
